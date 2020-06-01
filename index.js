d4305aef5890c28c44e90d68a4eb8df5899c00dc
Vanessa Rayyan <vanessarayyan@outlook.com>
2020-05-31 22:13:37 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function getDogImage() {
+    let number = $('#numberOfPics').val();
+    fetch('https://dog.ceo/api/breeds/image/random/'+number)
+    .then(response => response.json())
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
